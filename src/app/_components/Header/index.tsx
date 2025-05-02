b181836063aee426583c3d89adc71cd595861cff
faelcjeronymo <rafaelcjeronymo@outlook.com>
2025-05-02 16:42:21 -0300

    changed header border color

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return (
-        <header className="flex items-center justify-between p-4 border-b border-b-gray-300">
+        <header className="flex items-center justify-between p-4 border-b border-b-gray-200">
             <div className="bg-green-400" style={{width: 42, height: 42, borderRadius: 8}}></div>
         </header>
     );
