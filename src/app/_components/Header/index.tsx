c6a47ec671ed89dcc74d4897feb67ea3ef4e39c3
faelcjeronymo <rafaelcjeronymo@outlook.com>
2025-05-04 23:39:56 -0300

    changed header padding

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return (
-        <header className="flex items-center justify-between p-4 border-b border-b-gray-200">
+        <header className="flex items-center justify-between py-3 px-4 border-b border-b-gray-200">
             <div className="bg-green-400" style={{width: 42, height: 42, borderRadius: 8}}></div>
         </header>
     );
