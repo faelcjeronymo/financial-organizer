71a3dcff1ef8f08855c73bff2f810f2d2c2eb7a1
faelcjeronymo <rafaelcjeronymo@outlook.com>
2025-05-04 23:40:54 -0300

    style changes in search bar

--- src/app/_components/SearchBar/index.tsx
+++ src/app/_components/SearchBar/index.tsx
@@ -5,11 +5,10 @@ interface SearchBarProps {
 }
 
 const SearchBar = ({ placeholder }: SearchBarProps) => {
-
     return (
         <div className="relative">
-            <input className="p-3 py-2.5 border rounded-lg border-gray-300 ps-10 hover:border-gray-400 transition" placeholder={placeholder}/>
-            <Search className="absolute top-1/2 start-3 -translate-y-1/2 text-gray-500" size={20}/>
+            <input className="appearence-none p-3 py-2 border rounded-lg border-gray-300 ps-10 hover:border-gray-400 focus-visible:outline-0 transition h-[38]" placeholder={placeholder}/>
+            <Search className="absolute top-1/2 start-3 -translate-y-1/2 text-gray-400" size={18}/>
         </div>
     );
 }
