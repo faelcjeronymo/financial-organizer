7e6276be470bfdf3d8c17b2bc6b6982bfda848ba
faelcjeronymo <rafaelcjeronymo@outlook.com>
2025-05-06 23:26:22 -0300

    finished dropdown component

--- src/app/_components/Dropdown/index.tsx
+++ src/app/_components/Dropdown/index.tsx
@@ -2,7 +2,7 @@
 
 import { ChevronDown } from "lucide-react";
 import { ScriptProps } from "next/script";
-import { JSX, MouseEvent, ReactNode, useState } from "react";
+import React, { JSX, MouseEvent, ReactNode, useEffect, useRef, useState } from "react";
 
 interface DropdownProps {
     icon?: ReactNode,
@@ -10,38 +10,56 @@ interface DropdownProps {
     items: Array<DropdownMenuItemProps> | Array<DropdownGroup>
 }
 
-//TODO: Verificar se isso foi feito do jeito certo
 export interface DropdownGroup {
     title: string,
     items: Array<DropdownMenuItemProps>
 }
 
-//TODO: Verificar se isso foi feito do jeito certo
 export interface DropdownMenuItemProps {
     label: string,
     action?: (event: MouseEvent) => void,
     href?: string,
 }
 
-interface DropdownMenuProps {
+export interface DropdownMenuProps {
     children: ReactNode,
-    isClosed: boolean
+    isClosed: boolean,
+    ref: React.Ref<HTMLDivElement> | null,
 }
 
-interface DropdownButtonProps {
+export interface DropdownButtonProps {
+    ref: React.Ref<HTMLButtonElement> | null,
     children: ReactNode,
-    action: () => void,
+    action: (event?: MouseEvent<HTMLButtonElement>) => void,
 }
-
-//TODO: Colocar uma opção de fechar outros dropdown que estão abertos na página caso eu abra um
 
 const Dropdown = (props: DropdownProps) => {
     const { icon, label, items } = props;
     const [isClosed, setIsClosed] = useState(true);
-
+    const buttonRef = useRef<HTMLButtonElement | null>(null);
+    const menuRef = useRef<HTMLDivElement | null>(null);
     const menuItems: Array<JSX.Element> = [];
+
+    //TODO: Refactor and use useCallback hook
+    useEffect(() => {
+        function closeDropdownWithOutClick (this: Document, event: globalThis.MouseEvent) {
+            if (
+                (buttonRef.current && (event.target === buttonRef.current || buttonRef.current.contains(event.target as Node))) ||
+                (menuRef.current && menuRef.current.contains(event.target as Node))
+            ) {
+                return;
+            }
+            setIsClosed(true);
+        };
     
-    //TODO: Refatorar esse "items"/"currentItems", talvez aqui seja melhor eu chamar de "content"
+        document.addEventListener('click', closeDropdownWithOutClick);
+    
+        return () => {
+          document.removeEventListener('click', closeDropdownWithOutClick);
+        };
+      }, []);
+    
+    //TODO: Maybe will be better change "items"/"currentItems" to "content"
     for (const item in items) {
         if ('title' in items[item] && 'items' in items[item]) {
             const currentItem = items[item] as DropdownGroup;
@@ -68,14 +86,18 @@ const Dropdown = (props: DropdownProps) => {
 
     }
 
+    const closeOnButtonClick = () => {
+        setIsClosed(!isClosed);
+    }
+
     return (
         <div className="relative">
-            <DropdownButton action={() => {setIsClosed(!isClosed);}}>
+            <DropdownButton action={closeOnButtonClick} ref={buttonRef}>
                 {icon}
                 <div className="px-2 pe-1.5">{label}</div>
             </DropdownButton>
             {/* TODO: mapear os items através de um objeto */}
-            <DropdownMenu isClosed={isClosed}>
+            <DropdownMenu isClosed={isClosed} ref={menuRef}>
                 {menuItems}
             </DropdownMenu>
         </div>
@@ -86,7 +108,7 @@ const DropdownButton = (props: DropdownButtonProps) => {
     const { children, action } = props;
 
     return (
-        <button className="flex items-center p-3 py-2 h-[38] appearance-none border rounded-lg border-gray-300 bg-none hover:border-gray-400 focus-visible:outline-0 transition" onClick={action}>
+        <button className="flex items-center p-3 py-2 h-[38] appearance-none border rounded-lg border-gray-300 bg-none hover:border-gray-400 focus-visible:outline-0 transition" onClick={action} ref={props.ref}>
             {children}
             <ChevronDown className="text-gray-400" size={16} strokeWidth={2} style={{strokeWidth: 3}}/>
         </button>
@@ -100,13 +122,19 @@ const DropdownMenu = (props: DropdownMenuProps) => {
         "className": "absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in",
     };
 
+    if (!isClosed) {
+        menuProps.className = menuProps.className.replace("hidden", "block");
+    }
+
     return (
         isClosed ?
-        <div data-closed {...menuProps}>
-            {children}
+        <div data-closed {...menuProps} ref={props.ref}>
+            <div style={{visibility: "hidden"}}>
+                {children}
+            </div>
         </div>
         :
-        <div {...menuProps}>
+        <div {...menuProps} ref={props.ref}>
             {children}
         </div>
     );
